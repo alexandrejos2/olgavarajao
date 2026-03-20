d88b6f7872e2719d907d8f6e13fc29443a85c90d
alexandrejos2 <alexandrejos2@gmail.com>
2026-03-20 15:14:49 +0000

    Update tier order

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -25,13 +25,13 @@ const awards = [
   },
   {
     id: 5,
-    name: "Balão de Prata",
-    image: "https://res.cloudinary.com/dxrk58emm/image/upload/v1764936479/balao-prata-pb_msdjgv.png"
+    name: "Golden Club",
+    image: "https://res.cloudinary.com/dxrk58emm/image/upload/v1765469831/golden-club_jyxwcq.png"
   },
   {
     id: 6,
-    name: "Golden Club",
-    image: "https://res.cloudinary.com/dxrk58emm/image/upload/v1765469831/golden-club_jyxwcq.png"
+    name: "Balão de Prata",
+    image: "https://res.cloudinary.com/dxrk58emm/image/upload/v1764936479/balao-prata-pb_msdjgv.png"
   },
   {
     id: 7,
